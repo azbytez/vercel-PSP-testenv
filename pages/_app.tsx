7354c3cb5dc07cefdf1a7e456a952fad04a67414
azbytez <louiesolis1311@gmail.com>
2025-11-20 20:31:59 +0800

    Move tracking script to Head component as per instructions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,42 +1,44 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    // Matomo/Prospectory Tracking Code
-    (function(w, d) {
-      try {
-        if (w._psp) return;
-        var q = function() {
-          q.q = q.q || [];
-          q.q.push(arguments);
-        };
-        q.q = [];
-        w._psp = q;
-        var s = d.createElement("script");
-        s.async = 1;
-        s.src = "https://app-dev.prospectory.ai/psp.tracker.dev.js";
-        var f = d.getElementsByTagName("script")[0];
-        f.parentNode.insertBefore(s, f);
-        s.onload = function() {
-          try {
-            (w.tracking && w.tracking.load) && w.tracking.load({
-              pspClientId: "cmdymbogv000sna24m9ucukp4",
-              pspuserId: "cm470av0g0000cyvsydoy9jrd"
-            });
-          } catch (e) {}
-        };
-      } catch (e) {}
-    })(window, document);
-  }, [])
-
   return (
     <>
       <Head>
         <title>Domain Automation Dashboard</title>
         <meta name="description" content="Domain Automation Dashboard" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Matomo/Prospectory Tracking Code - Added in head section as per instructions */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w, d) {
+                try {
+                  if (w._psp) return;
+                  var q = function() {
+                    q.q = q.q || [];
+                    q.q.push(arguments);
+                  };
+                  q.q = [];
+                  w._psp = q;
+                  var s = d.createElement("script");
+                  s.async = 1;
+                  s.src = "https://app-dev.prospectory.ai/psp.tracker.dev.js";
+                  var f = d.getElementsByTagName("script")[0];
+                  f.parentNode.insertBefore(s, f);
+                  s.onload = function() {
+                    try {
+                      (w.tracking && w.tracking.load) && w.tracking.load({
+                        pspClientId: "cmdymbogv000sna24m9ucukp4",
+                        pspuserId: "cm470av0g0000cyvsydoy9jrd"
+                      });
+                    } catch (e) {}
+                  };
+                } catch (e) {}
+              })(window, document);
+            `,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </>
