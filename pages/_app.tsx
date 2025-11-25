ab3586c7432e2bb319ed2431fe8ef37211928d52
azbytez <louiesolis1311@gmail.com>
2025-11-25 10:56:20 +0800

    Update tracking code with new Client ID and User ID, update title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Domain Automation Dashboard</title>
-        <meta name="description" content="Domain Automation Dashboard" />
+        <title>Client Site Tracker Test</title>
+        <meta name="description" content="Client Site Tracker Test" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* Matomo/Prospectory Tracking Code - Added in head section as per instructions */}
         <script
@@ -16,22 +16,16 @@ export default function App({ Component, pageProps }: AppProps) {
                 try {
                   if (w._psp) return;
                   var q = function() {
-                    q.q = q.q || [];
-                    q.q.push(arguments);
+                    q.q.push(arguments)
                   };
-                  q.q = [];
-                  w._psp = q;
-                  var s = d.createElement("script");
-                  s.async = 1;
-                  s.src = "https://app-dev.prospectory.ai/psp.tracker.dev.js";
-                  var f = d.getElementsByTagName("script")[0];
-                  f.parentNode.insertBefore(s, f);
+                  q.q = [];w._psp = q;var s = d.createElement("script");s.async = 1;s.src = "https://app-dev.prospectory.ai/psp.tracker.dev.js";
+                  var f = d.getElementsByTagName("script")[0];f.parentNode.insertBefore(s, f);
                   s.onload = function() {
                     try {
                       (w.tracking && w.tracking.load) && w.tracking.load({
-                        pspClientId: "cmdymbogv000sna24m9ucukp4",
-                        pspuserId: "cm470av0g0000cyvsydoy9jrd"
-                      });
+                        pspClientId: "cmdymbp3q001cna243vesyx4r",
+                        pspUserId: "cm6rgwls50000jb2lypjf341q"
+                      })
                     } catch (e) {}
                   };
                 } catch (e) {}
